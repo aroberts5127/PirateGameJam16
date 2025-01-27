22c5c72ef3afb0fea240357e576bb150515f8cb7
Andrew Roberts <aroberts5127@gmail.com>
2025-01-26 18:16:13 -0800

    Addition of TMP to begin work on Dialogue System, and Dialogue UI

---
+++ Assets/Scripts/UI/CutsceneDialogueController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CutsceneDialogueController : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI characterNameTMP;
+    [SerializeField]
+    private TextMeshProUGUI characterDialogueTMP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
